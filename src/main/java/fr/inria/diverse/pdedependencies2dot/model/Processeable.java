725d05565e1205d2ff815673836b29a24bbed754
Erwan Bousse <erwan.bousse@irisa.fr>
2015-08-14 18:44:41 +0200

    Forgot to commit some stuff for the metamodel

---
+++ src/main/java/fr/inria/diverse/pdedependencies2dot/model/Processeable.java
@@ -0,0 +1,50 @@
+/**
+ */
+package fr.inria.diverse.pdedependencies2dot.model;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Processeable</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link fr.inria.diverse.pdedependencies2dot.model.Processeable#isProcessed <em>Processed</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see fr.inria.diverse.pdedependencies2dot.model.ModelPackage#getProcesseable()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Processeable extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Processed</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Processed</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Processed</em>' attribute.
+	 * @see #setProcessed(boolean)
+	 * @see fr.inria.diverse.pdedependencies2dot.model.ModelPackage#getProcesseable_Processed()
+	 * @model
+	 * @generated
+	 */
+	boolean isProcessed();
+
+	/**
+	 * Sets the value of the '{@link fr.inria.diverse.pdedependencies2dot.model.Processeable#isProcessed <em>Processed</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Processed</em>' attribute.
+	 * @see #isProcessed()
+	 * @generated
+	 */
+	void setProcessed(boolean value);
+
+} // Processeable
---
+++ src/main/java/fr/inria/diverse/pdedependencies2dot/model/impl/ProcesseableImpl.java
@@ -0,0 +1,163 @@
+/**
+ */
+package fr.inria.diverse.pdedependencies2dot.model.impl;
+
+import fr.inria.diverse.pdedependencies2dot.model.ModelPackage;
+import fr.inria.diverse.pdedependencies2dot.model.Processeable;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Processeable</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link fr.inria.diverse.pdedependencies2dot.model.impl.ProcesseableImpl#isProcessed <em>Processed</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class ProcesseableImpl extends MinimalEObjectImpl.Container implements Processeable {
+	/**
+	 * The default value of the '{@link #isProcessed() <em>Processed</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isProcessed()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final boolean PROCESSED_EDEFAULT = false;
+
+	/**
+	 * The cached value of the '{@link #isProcessed() <em>Processed</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isProcessed()
+	 * @generated
+	 * @ordered
+	 */
+	protected boolean processed = PROCESSED_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ProcesseableImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ModelPackage.Literals.PROCESSEABLE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public boolean isProcessed() {
+		return processed;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setProcessed(boolean newProcessed) {
+		boolean oldProcessed = processed;
+		processed = newProcessed;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ModelPackage.PROCESSEABLE__PROCESSED, oldProcessed, processed));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case ModelPackage.PROCESSEABLE__PROCESSED:
+				return isProcessed();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case ModelPackage.PROCESSEABLE__PROCESSED:
+				setProcessed((Boolean)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case ModelPackage.PROCESSEABLE__PROCESSED:
+				setProcessed(PROCESSED_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case ModelPackage.PROCESSEABLE__PROCESSED:
+				return processed != PROCESSED_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (processed: ");
+		result.append(processed);
+		result.append(')');
+		return result.toString();
+	}
+
+} //ProcesseableImpl
